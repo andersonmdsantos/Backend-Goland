fafbcb39b5758f03a890f84e3df6bc01b4059822
Anderson Santos <88168152+andersonmdsantos@users.noreply.github.com>
2025-06-11 11:47:21 -0300

    Adição de funções de pacotes, tipos de variaveis e constantes

---
+++ modulo_4/varconst.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome string
+	nome = "Anderson"
+	fmt.Println(nome)
+
+	nome = "Santos"
+	fmt.Println(nome)
+
+	var b, c int = 2, 3
+	fmt.Println(b, c)
+
+	var a, d = 4, 10
+	fmt.Println(a, d)
+
+	var e = true
+	fmt.Println(e)
+
+	f := "Apple"
+	fmt.Println(f)
+
+	const fruta = "Maça"
+	fmt.Println(fruta)
+}
